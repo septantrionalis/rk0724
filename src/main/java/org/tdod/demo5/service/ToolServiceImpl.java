2b585cf2403c5c3e7d72438a92ca02b9399ab731
Ron <rkinney@tdod.org>
2024-07-09 15:06:44 -0600

    calculate holidays

--- src/main/java/org/tdod/demo5/service/ToolServiceImpl.java
+++ src/main/java/org/tdod/demo5/service/ToolServiceImpl.java
@@ -8,7 +8,9 @@ import org.tdod.demo5.entity.ToolTypeEnum;
 import org.tdod.demo5.repository.ToolRepository;
 
 import java.math.BigDecimal;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -35,15 +37,15 @@ public class ToolServiceImpl implements ToolService {
         rentalAgreement.setCheckoutDate(checkoutDate);
         rentalAgreement.setDueDate(calculateDueDate(rentalDayCount, checkoutDate));
         rentalAgreement.setDailyRentalCharge(calculateDailyRentalCharge(tool));
+        rentalAgreement.setChargeDays(calculateChargeDays(checkoutDate, rentalAgreement.getDueDate()));
 
-        calculateChargeDays(checkoutDate, rentalAgreement.getDueDate());
 
 
         return rentalAgreement;
     }
 
     private LocalDate calculateDueDate(int rentalDayCount, LocalDate checkoutDate) {
-        return checkoutDate.plusDays(rentalDayCount);
+        return checkoutDate.plusDays(rentalDayCount - 1);
     }
 
     private BigDecimal calculateDailyRentalCharge(Tool tool) {
@@ -54,15 +56,38 @@ public class ToolServiceImpl implements ToolService {
         LocalDate currentDate = LocalDate.of(checkoutDate.getYear(), checkoutDate.getMonth(), checkoutDate.getDayOfMonth());
         LocalDate endDate = LocalDate.of(dueDate.getYear(), dueDate.getMonth(), dueDate.getDayOfMonth());;;
 
+        int chargeDays = 0;
         while (!currentDate.isAfter(endDate)) {
-            System.out.println(currentDate);
+            System.out.println("Evaluating " + currentDate);
             currentDate = currentDate.plusDays(1);
+            if (!isHoliday(currentDate)) {
+                chargeDays++;
+            }
         }
 
-        return 0;
+        return chargeDays;
     }
 
-    private void isHoliday() {
+    private boolean isHoliday(LocalDate date) {
+        // Independence Day
+        LocalDate independenceDay = LocalDate.of(date.getYear(), Month.JULY, 4);
+        if (date.equals(independenceDay)) {
+            System.out.println("Holiday found for independence day: " + date);
+            return true;
+        }
+
+        // Labor Day : First Monday in September
+        int currentYear = date.getYear();
+        LocalDate firstMondayInSeptember = LocalDate.of(currentYear, Month.SEPTEMBER, 1);
+        while (firstMondayInSeptember.getDayOfWeek() != DayOfWeek.MONDAY) {
+            firstMondayInSeptember = firstMondayInSeptember.plusDays(1);
+        }
+        if (date.equals(firstMondayInSeptember)) {
+            System.out.println("Holiday found for labor day: " + date);
+            return true;
+        }
+
+        return false;
 
     }
 
